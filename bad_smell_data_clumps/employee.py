07057891cca8a7c64e64e64d6bab5122d45c2bad
joe3huang <joe3huang@gmail.com>
2017-04-29 21:54:26 +1200

    Employee class refactoring and testing

--- bad_smell_data_clumps/employee.py
+++ bad_smell_data_clumps/employee.py
@@ -5,13 +5,14 @@ from datetime import date, datetime
 class Employee(object):
 
     def __init__(self, parameter_object):
-        self.id = parameter_object['Id']
-        self.gender = parameter_object['Gender']
-        self.age = parameter_object['Age']
-        self.sales = parameter_object['Sales']
-        self.BMI = parameter_object['BMI']
-        self.salary = parameter_object['Salary']
-        self.birthday = datetime.strptime(parameter_object['Birthday'], '%d-%m-%Y')
+        self.parameter_object = parameter_object
+        self.id = self.parameter_object['Id']
+        self.gender = self.parameter_object['Gender']
+        self.age = self.parameter_object['Age']
+        self.sales = self.parameter_object['Sales']
+        self.BMI = self.parameter_object['BMI']
+        self.salary = self.parameter_object['Salary']
+        self.birthday = datetime.strptime(self.parameter_object['Birthday'], '%d-%m-%Y')
 
     def __str__(self):
         txt = self.id + ',' + self.gender + ',' + \
